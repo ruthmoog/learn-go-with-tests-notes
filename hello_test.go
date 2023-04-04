a376171603d1c30f3ee89364517f3202f007a1be
Ruth Baker <ruthmoog@gmail.com>
2023-04-04 10:22:37 +0100

    Keep going! More requirements...

--- hello_test.go
+++ hello_test.go
@@ -6,13 +6,18 @@ import (
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Ruth")
+		got := Hello("Ruth", "")
 		want := "Hello, Ruth!"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
 		assertCorrectMessage(t, got, want)
 	})
 }
