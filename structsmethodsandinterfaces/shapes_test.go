964bce9c8db0c70b0d7ca00f478499244d88318e
Ruth Baker <ruthmoog@gmail.com>
2023-04-11 17:45:13 +0100

    structs, methods and interfaces: triangle

--- structsmethodsandinterfaces/shapes_test.go
+++ structsmethodsandinterfaces/shapes_test.go
@@ -1,6 +1,8 @@
 package structsmethodsandinterfaces
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -15,17 +17,21 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name         string
+		shape        Shape
+		expectedArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, expectedArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, expectedArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, expectedArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("wanted %.2f, but got %.2f", tt.want, got)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.expectedArea {
+				t.Errorf("%#v wanted area %.2f, but got %.2f", tt.shape, tt.expectedArea, got)
+			}
+		})
 	}
 }
