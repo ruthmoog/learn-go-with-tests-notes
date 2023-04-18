df8f101965b5fa8c0c2b3c33dabc406fa4f97067
Ruth Baker <ruthmoog@gmail.com>
2023-04-18 10:24:31 +0100

    mend

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -7,9 +7,13 @@ func TestSearch(t *testing.T) {
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("wanted %q but got %q", want, got)
 	}
-
 }
