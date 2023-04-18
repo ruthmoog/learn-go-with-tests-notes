6d215768eafa794f849d2d649dbf22b93d13637c
Ruth Baker <ruthmoog@gmail.com>
2023-04-18 10:37:54 +0100

    maps: handling unknown words

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,15 @@
 package maps
 
+import "errors"
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,19 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -16,5 +25,13 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("wanted %q but got %q", want, got)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("wanted %q but got error %q", want, got)
 	}
 }
