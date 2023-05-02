ea995987897e63cf854f027733ad766d2ca98d3d
Ruth Baker <ruthmoog@gmail.com>
2023-05-02 14:35:38 +0100

    reflection: struct with two string fields

--- reflection/walker_test.go
+++ reflection/walker_test.go
@@ -1,23 +1,44 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
-	x := struct {
-		Name string
-	}{expected}
 
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, expected %d but got %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         any
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "Ankara"},
+			[]string{"Chris", "Ankara"},
+		},
 	}
 
-	if got[0] != expected {
-		t.Errorf("expected %q but got %q", expected, got[0])
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("expected %v but got %v", test.ExpectedCalls, got)
+			}
+		})
 	}
 }
