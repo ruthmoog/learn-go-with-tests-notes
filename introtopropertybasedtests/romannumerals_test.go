f471ade02898b6b3b9005a9a47eac885c41b2c33
Ruth Baker <ruthmoog@gmail.com>
2023-07-06 11:21:51 +0100

    PropertyBasedTests: Roman numerals I - IV

---
+++ introtopropertybasedtests/romannumerals_test.go
@@ -0,0 +1,26 @@
+package introtopropertybasedtests
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, but wanted %q", got, test.Want)
+			}
+		})
+	}
+}
