e498a3a41361fe9b0200eb9958a28c5ba5a8a034
Ruth Baker <ruthmoog@gmail.com>
2023-07-06 14:59:40 +0100

    PropertyBasedTests: Refactor roman numerals type

--- introtopropertybasedtests/romannumerals.go
+++ introtopropertybasedtests/romannumerals.go
@@ -2,12 +2,23 @@ package introtopropertybasedtests
 
 import "strings"
 
+type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+	return 0
+}
+
 type RomanNumeral struct {
 	Value  int
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral{
+var allRomanNumerals = RomanNumerals{
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -39,8 +50,28 @@ func ConvertToRoman(arabic int) string {
 
 func ConvertToArabic(roman string) int {
 	total := 0
-	for range roman {
-		total++
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if couldBeSubtractive(i, symbol, roman) {
+			nextSymbol := roman[i+1]
+			potentialNumber := string([]byte{symbol, nextSymbol})
+			value := allRomanNumerals.ValueOf(potentialNumber)
+
+			if value != 0 {
+				total += value
+				i++
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
 	}
 	return total
+}
+
+func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
+	return index+1 < len(roman) && currentSymbol == 'I'
 }
