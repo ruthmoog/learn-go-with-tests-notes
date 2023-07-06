f1a19fbf52721956a9b86f6f0ecba9d375d21daa
Ruth Baker <ruthmoog@gmail.com>
2023-07-06 11:35:33 +0100

    PropertyBasedTests: Roman numerals XIV, XVIII, XX and XXXIX

--- introtopropertybasedtests/romannumerals_test.go
+++ introtopropertybasedtests/romannumerals_test.go
@@ -19,6 +19,10 @@ func TestRomanNumerals(t *testing.T) {
 		{"8 gets converted to VIII", 8, "VIII"},
 		{"9 gets converted to IX", 9, "IX"},
 		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"18 gets converted to XVIII", 18, "XVIII"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
