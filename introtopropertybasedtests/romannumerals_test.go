3ad91efcc42f3c382ffab156bf2d5441d79b882f
Ruth Baker <ruthmoog@gmail.com>
2023-07-06 11:31:52 +0100

    PropertyBasedTests: Roman numerals IX

--- introtopropertybasedtests/romannumerals_test.go
+++ introtopropertybasedtests/romannumerals_test.go
@@ -17,6 +17,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"6 gets converted to VI", 6, "VI"},
 		{"7 gets converted to VII", 7, "VII"},
 		{"8 gets converted to VIII", 8, "VIII"},
+		{"9 gets converted to IX", 9, "IX"},
 	}
 
 	for _, test := range cases {
