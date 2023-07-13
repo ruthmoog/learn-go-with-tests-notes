331306d145434f2a5d0e558d099d991d57a70aa0
Ruth Baker <ruthmoog@gmail.com>
2023-07-13 13:21:14 +0100

    Math: Refactor test SVG writer 00:00

--- mathclockface/acceptance_test.go
+++ mathclockface/acceptance_test.go
@@ -8,50 +8,50 @@ import (
 	"time"
 )
 
-type Svg struct {
+type SVG struct {
 	XMLName xml.Name `xml:"svg"`
-	Text    string   `xml:",chardata"`
 	Xmlns   string   `xml:"xmlns,attr"`
 	Width   string   `xml:"width,attr"`
 	Height  string   `xml:"height,attr"`
 	ViewBox string   `xml:"viewBox,attr"`
 	Version string   `xml:"version,attr"`
-	Circle  struct {
-		Text  string `xml:",chardata"`
-		Cx    string `xml:"cx,attr"`
-		Cy    string `xml:"cy,attr"`
-		R     string `xml:"r,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"circle"`
-	Line []struct {
-		Text  string `xml:",chardata"`
-		X1    string `xml:"x1,attr"`
-		Y1    string `xml:"y1,attr"`
-		X2    string `xml:"x2,attr"`
-		Y2    string `xml:"y2,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"line"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
 }
 
 func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
 	b := bytes.Buffer{}
 	mathclockface.SVGWriter(&b, tm)
+	svg := SVG{}
 
-	svg := Svg{}
-	xml.Unmarshal(b.Bytes(), &svg)
+	err := xml.Unmarshal(b.Bytes(), &svg)
+	if err != nil {
+		return
+	}
 
-	x2 := "150.000"
-	y2 := "60.000"
+	want := Line{150, 150, 150, 60}
 
 	for _, line := range svg.Line {
-		if line.X2 == x2 && line.Y2 == y2 {
+		if line == want {
 			return
 		}
 	}
 
-	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
+	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %v", want, svg.Line)
 }
 
 func TestSecondHandAtMidnight(t *testing.T) {
