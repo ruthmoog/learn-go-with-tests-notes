b7c1e7b9a4fbcc0c971e24d935b97d068db51f35
Ruth Baker <ruthmoog@gmail.com>
2023-07-13 10:47:28 +0100

    Math: use rough equality for seconds hand point

--- mathclockface/mathclockface_test.go
+++ mathclockface/mathclockface_test.go
@@ -41,14 +41,25 @@ func TestSecondHandPoint(t *testing.T) {
 		point Point
 	}{
 		{simpleTime(0, 0, 30), Point{0, -1}},
+		{simpleTime(0, 0, 45), Point{-1, 0}},
 	}
 
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
 			got := secondHandPoint(c.time)
-			if got != c.point {
+			if !roughlyEqualPoint(got, c.point) {
 				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
 			}
 		})
 	}
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) &&
+		roughlyEqualFloat64(a.Y, b.Y)
+}
+
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7 // within 0.0000001
+	return math.Abs(a-b) < equalityThreshold
 }
