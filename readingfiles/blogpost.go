9d2b36ea6c351ed22ec38840d15bb3e03862ab8e
Ruth Baker <ruthmoog@gmail.com>
2023-07-27 13:24:40 +0100

    Reading Files: handle reader failure

--- readingfiles/blogpost.go
+++ readingfiles/blogpost.go
@@ -4,13 +4,18 @@ import (
 	"io/fs"
 )
 
-func PostsFromFS(fileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
+func PostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+
+	if err != nil {
+		return nil, err
+	}
+
 	var posts []Post
 	for _, f := range dir {
 		posts = append(posts, makePostFromFile(fileSystem, f.Name()))
 	}
-	return posts
+	return posts, nil
 }
 
 func makePostFromFile(fileSystem fs.FS, fileName string) Post {
--- readingfiles/blogpost_test.go
+++ readingfiles/blogpost_test.go
@@ -1,23 +1,45 @@
 package readingfiles
 
 import (
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 )
 
 func TestBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello-world.md": {Data: []byte("Title: Hello, World!")},
-		//"hello-twitch.md": {Data: []byte("Title: Hello, Twitch!")},
-	}
-	posts := PostsFromFS(fs)
 
-	if len(posts) != len(fs) {
-		t.Errorf("Expected %d posts, got %d posts", len(fs), len(posts))
-	}
+	t.Run("happy path", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello-world.md": {Data: []byte("Title: Hello, World!")},
+			//"hello-twitch.md": {Data: []byte("Title: Hello, Twitch!")},
+		}
+		posts, err := PostsFromFS(fs)
 
-	expectedFirstPost := Post{Title: "Hello, World!"}
-	if posts[0] != expectedFirstPost {
-		t.Errorf("expected %#v, but got %#v", expectedFirstPost, posts[0])
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(posts) != len(fs) {
+			t.Errorf("Expected %d posts, got %d posts", len(fs), len(posts))
+		}
+
+		expectedFirstPost := Post{Title: "Hello, World!"}
+		if posts[0] != expectedFirstPost {
+			t.Errorf("expected %#v, but got %#v", expectedFirstPost, posts[0])
+		}
+	})
+
+	t.Run("Error handling", func(t *testing.T) {
+		_, err := PostsFromFS(FailingFS{})
+		if err == nil {
+			t.Error("expected error but didnt get one")
+		}
+	})
+}
+
+type FailingFS struct{}
+
+func (f FailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("I always fail")
 }
