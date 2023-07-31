8e087e2ae0281f6679f00116d681d0a08e579b21
Ruth Baker <ruthmoog@gmail.com>
2023-07-31 15:32:25 +0100

    Generics: Assert on strings

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -7,20 +7,23 @@ func TestAssertFunctions(t *testing.T) {
 		AssertEqual(t, 1, 1)
 		AssertNotEqual(t, 1, 2)
 	})
+
+	t.Run("assert on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "goodbye")
+	})
 }
 
-func AssertEqual(t *testing.T, got, want int) {
-
+func AssertEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %d, want %d", got, want)
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want int) {
+func AssertNotEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got == want {
-		t.Errorf("didn't want %d", got)
+		t.Errorf("didn't want %v", got)
 	}
-
 }
