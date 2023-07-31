4ec3dab509cb86582fa85514fb9cfa018e2fea59
Ruth Baker <ruthmoog@gmail.com>
2023-07-31 15:56:35 +0100

    Generics: Compare types with `comparable`

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -12,16 +12,18 @@ func TestAssertFunctions(t *testing.T) {
 		AssertEqual(t, "hello", "hello")
 		AssertNotEqual(t, "hello", "goodbye")
 	})
+
+	//AssertEqual(t, 1, "1") // uncomment to see the error
 }
 
-func AssertEqual(t *testing.T, got, want interface{}) {
+func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want interface{}) {
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got == want {
 		t.Errorf("didn't want %v", got)
