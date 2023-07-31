622d26aee5778b90139d9782f1d7b59aaa4c7e11
Ruth Baker <ruthmoog@gmail.com>
2023-07-31 20:19:52 +0100

    Generics: Test stack using generic types

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -16,6 +16,45 @@ func TestAssertFunctions(t *testing.T) {
 	//AssertEqual(t, 1, "1") // uncomment to see the error
 }
 
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(Stack[int])
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(99)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(1)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 1)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 99)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		AssertEqual(t, firstNum+secondNum, 3)
+
+	})
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
+
 func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
